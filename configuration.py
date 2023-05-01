ca807ccd293fb8036693c13f0e372bc485568134
Segev Ben Hayon <segev.benhayon@se.com>
2023-05-01 17:35:57 -0400

    change output to be via REST API web server

--- configuration.py
+++ configuration.py
@@ -1,6 +1,1 @@
 version = "1.0.0"
-host_path = "./results"
-local_path = "/app/results"
-file_name = "version.txt"
-update_interval = 1
-stop = "no"
--- main.py
+++ main.py
@@ -1,77 +1,15 @@
-import importlib
-import os
-from time import sleep
 import configuration
+import socket
+from flask import Flask, request, jsonify
 
-class PROCSingleton(type):
-  _instances = {}
-  def __call__(cls, *args, **kwargs):
-    if cls not in cls._instances:
-      cls._instances[cls] = super(PROCSingleton, cls).__call__(*args, **kwargs)
-    return cls._instances[cls]
+app = Flask(__name__)
 
-class process_k8s_ver_demo(metaclass = PROCSingleton):
-  def __init__(self):
-    self.path = configuration.local_path
-    self.file = configuration.file_name
+@app.route('/', methods=['GET'])
+def index():
+    hostname = socket.gethostname()
+    ipaddress = socket.gethostbyname(hostname)
+    response = {"status": "success", "version": configuration.version, "hostname": hostname, "IP address": ipaddress}
+    return jsonify(response)
 
-  def createPath (self, path):
-    if not os.path.exists(path):
-      os.makedirs(path)
-
-  def updateVersion (self, filePath):
-    f = open(filePath, 'w')
-    f.write(self.version)
-    f.close()
-
-  def createFile (self, filePath):
-    if os.path.isfile(filePath) == False:
-      print(f'{filePath} not exist')
-      self.updateVersion(filePath)
-
-  def updateFile (self, filePath):
-    with open(filePath) as f:
-      try:
-        major_f, minor_f, patch_f = [int(x) for x in next(f).split('.')]
-      except:
-        print(f'No version or wrong versiobn format in the file [{filePath}]')
-
-    try:
-      major_c, minor_c, patch_c = [int(x) for x in self.version.split('.')]
-    except:
-      print(f'No version or wrong versiobn format in the configuration file')
-
-    if major_c > major_f:
-      print(f'{major_c}.{minor_c}.{patch_c} > {major_f}.{minor_f}.{patch_f} - update now')
-      self.updateVersion(filePath)
-    elif major_c == major_f and minor_c > minor_f:
-      print(f'{major_c}.{minor_c}.{patch_c} > {major_f}.{minor_f}.{patch_f} - update now')
-      self.updateVersion(filePath)
-    elif major_c == major_f and minor_c == minor_f and patch_c > patch_f:
-      print(f'{major_c}.{minor_c}.{patch_c} > {major_f}.{minor_f}.{patch_f} - update now')
-      self.updateVersion(filePath)
-
-
-  def run(self):
-    self.createPath(self.path)
-    running = True
-    while running:
-      importlib.reload(configuration)
-      self.version = configuration.version
-      self.interval = configuration.update_interval
-      self.stop = configuration.stop
-      if self.stop == "yes":
-        running = False
-    
-      self.createFile(self.path+"/"+self.file)
-      #self.updateFile(self.path+"/"+self.file)
-      self.updateVersion(self.path+"/"+self.file)
-
-      sleep(self.interval)
-
-def main():
-  PAdemo_proc = process_k8s_ver_demo()
-  PAdemo_proc.run()
-
-if __name__=='__main__':
-  main()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
